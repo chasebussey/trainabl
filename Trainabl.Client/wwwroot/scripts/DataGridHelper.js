c219d226eff927c229bbffd776d052cb34825853
pryde <pryde.dev@gmail.com>
2022-11-30 15:53:52 -0600

    Remove console.log

--- Trainabl.Client/wwwroot/scripts/DataGridHelper.js
+++ Trainabl.Client/wwwroot/scripts/DataGridHelper.js
@@ -1,6 +1,5 @@
 function FocusLastRow() {
     var rows = document.getElementsByName("movementName");
     var lastRow = rows.item(rows.length - 1);
-    console.log("APP: interop called")
     lastRow.focus();
 }
